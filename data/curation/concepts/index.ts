c110ceb5a4be942a3875d9f7690413b8c6c70dc2
danielbrydenjohnson <danielbrydenjohnson@gmail.com>
2026-05-30 12:18:27 +0100

    Move water concept data to curation layer

--- data/curation/concepts/index.ts
+++ data/curation/concepts/index.ts
@@ -1,6 +1,6 @@
-import { motherConcept } from "@/data/concepts/mother";
-import { waterConcept } from "@/data/concepts/water";
 import { nightConcept } from "@/data/curation/concepts/night";
+import { motherConcept } from "@/data/curation/concepts/mother";
+import { waterConcept } from "@/data/curation/concepts/water";
 import type { CognateConcept } from "@/data/concepts/types";
 
 export const curatedConcepts = [
